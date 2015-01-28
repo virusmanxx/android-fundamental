68b88b18d478967ab6b785a8e5c68e6c3733a3a2
Jerry <jerry.cm@blake-dair.com>
2015-01-28 15:04:18 +0700

    Added listview example

--- app/src/main/java/cpe/phaith/androidfundamental/MainActivity.java
+++ app/src/main/java/cpe/phaith/androidfundamental/MainActivity.java
@@ -1,17 +1,54 @@
 package cpe.phaith.androidfundamental;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cpe.phaith.androidfundamental.adapters.PhoneListAdapter;
+import cpe.phaith.androidfundamental.items.PhoneItem;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Context context;
+    private ListView listView;
+    private List<PhoneItem> phoneList;
+    private PhoneListAdapter phoneListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+        listView = (ListView)findViewById(R.id.listView);
+
+        phoneList = new ArrayList<>();
+        phoneListAdapter = new PhoneListAdapter(context, phoneList);
+        listView.setAdapter(phoneListAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                PhoneListAdapter adapter = (PhoneListAdapter)parent.getAdapter();
+                Toast.makeText(context,adapter.getItem(position).getName()+" is selected",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        phoneList.add(new PhoneItem("Galaxy Nexus", null));
+        phoneList.add(new PhoneItem("Nexus 4", null));
+        phoneList.add(new PhoneItem("Nexus 5", null));
+        phoneList.add(new PhoneItem("Iphone 5", null));
+
+
     }
 
 
---
+++ app/src/main/java/cpe/phaith/androidfundamental/items/PhoneItem.java
@@ -0,0 +1,27 @@
+package cpe.phaith.androidfundamental.items;
+
+public class PhoneItem {
+    private String name;
+    private String imageUrl;
+
+    public PhoneItem(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
