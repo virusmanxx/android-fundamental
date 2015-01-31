f49402fafe406bf01a3443bebcfebcac8d233ac3
Phaith <alongkot.g39@gmail.com>
2015-02-01 03:41:23 +0700

    Added pubnub

--- app/src/main/java/cpe/phaith/androidfundamental/MainActivity.java
+++ app/src/main/java/cpe/phaith/androidfundamental/MainActivity.java
@@ -2,16 +2,98 @@ package cpe.phaith.androidfundamental;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.pubnub.api.Callback;
+import com.pubnub.api.Pubnub;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+    private static final String NEW_LINE = System.getProperty("line.separator");
+    private TextView txtChat;
+    private Button btnSend;
+    private EditText chatInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtChat = (TextView) findViewById(R.id.txtChat);
+        btnSend = (Button) findViewById(R.id.btnSend);
+        chatInput = (EditText) findViewById(R.id.chatInput);
+
+        final Pubnub pubnub = new Pubnub("demo", "demo");
+        try {
+            pubnub.subscribe(new String[]{"hello_channel"}, new Callback() {
+                @Override
+                public void successCallback(String channel, final Object message) {
+                    super.successCallback(channel, message);
+                    Log.i(TAG, "pubnub success: " + message.toString());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            txtChat.append(message.toString() + NEW_LINE);
+                        }
+                    });
+                }
+
+                @Override
+                public void errorCallback(String channel, Object message) {
+                    super.errorCallback(channel, message);
+                    Log.e(TAG, "pubnub error: " + message.toString());
+                }
+
+                @Override
+                public void connectCallback(String channel, Object message) {
+                    super.connectCallback(channel, message);
+                    Log.i(TAG, "pubnub connect: " + message.toString());
+                }
+
+                @Override
+                public void reconnectCallback(String channel, Object message) {
+                    super.reconnectCallback(channel, message);
+                    Log.i(TAG, "pubnub reconnect: " + message.toString());
+                }
+
+                @Override
+                public void disconnectCallback(String channel, Object message) {
+                    super.disconnectCallback(channel, message);
+                    Log.i(TAG, "pubnub disconnect: " + message.toString());
+                }
+            });
+        } catch (Exception e) {
+            Log.e(TAG, "pubnub exception", e);
+        }
+
+        final Callback callback = new Callback() {
+            @Override
+            public void successCallback(String channel, Object message) {
+                super.successCallback(channel, message);
+                Log.i(TAG, "publish success: " + message.toString());
+            }
+
+            @Override
+            public void errorCallback(String channel, Object message) {
+                super.errorCallback(channel, message);
+                Log.e(TAG, "publish error: " + message.toString());
+            }
+        };
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pubnub.publish("hello_channel", chatInput.getText().toString(), callback);
+            }
+        });
+
     }
 
 
