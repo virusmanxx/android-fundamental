6000d342734f811a77c127062d3a7604d28aac56
Phaith <alongkot.g39@gmail.com>
2015-02-01 03:01:17 +0700

    Added sharedPreferences

--- app/src/main/java/cpe/phaith/androidfundamental/MainActivity.java
+++ app/src/main/java/cpe/phaith/androidfundamental/MainActivity.java
@@ -1,17 +1,52 @@
 package cpe.phaith.androidfundamental;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Context context;
+    private Button btnSave;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+        btnSave = (Button)findViewById(R.id.btnSave);
+        editText = (EditText)findViewById(R.id.username);
+        editText.setText(getText("username"));
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveText("username", editText.getText().toString());
+            }
+        });
+    }
+
+    private void saveText(String key, String text){
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(key, text);
+        editor.commit();
+        Toast.makeText(context, "username is saved.", Toast.LENGTH_SHORT).show();
+    }
+
+    private String getText(String key){
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        return sharedPref.getString(key,"");
     }
 
 
